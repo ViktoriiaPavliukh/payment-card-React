c1c2dde5f20f5cf388d7625d97539d629a779178
ViktoriiaPavliukh <victoriataran.mail@gmail.com>
2023-11-02 12:57:54 +0000

    add content to back-card, update position

--- src/App.js
+++ src/App.js
@@ -13,21 +13,25 @@ function App() {
             <img src={bgMobile} alt="bg-mobile" />
           </picture>
         </div>
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-          <div className="mt-10 mx-5">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 max-w-7xl mx-auto">
+          <div className="mt-10 mx-5 lg:grid lg:grid-cols lg:gap-8">
             <article className="front-card p-5 flex flex-col justify-between">
               <img src={logo} alt="logo" className="w-20 lg:w-28" />
               <div className="text-white">
                 <h2 className="text-xl mb-6  tracking-widest lg:text-4xl">
                   1234 5678 9012 3456
                 </h2>
-                <ul className="flex items-center justify-between uppercase text-xl tracking-widest">
-                  <li className="">Thomas Sankara</li>
+                <ul className="flex items-center justify-between uppercase text-xl tracking-widest text-base lg:text-xl">
+                  <li>Thomas Sankara</li>
                   <li>00/00</li>
                 </ul>
               </div>
             </article>
-            <article className="back-card"></article>
+            <article className="back-card relative lg:ml-20">
+              <p className="absolute right-10 text-xl text-white tracking-widest">
+                123
+              </p>
+            </article>
           </div>
           <div>form</div>
         </div>
